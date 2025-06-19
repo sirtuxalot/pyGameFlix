e4db542a05b36c26048722191d7018c5c22943ad
G. J. Yeomans <n8hth@protonmail.com>
2025-06-19 11:30:44 -0400

    Add seed data for users and subscriptions

--- seed_data.py
+++ seed_data.py
@@ -19,3 +19,24 @@ def seed_consoles(db_server, consoles_db):
   ]
   db_server.session.add_all(console_objects)
   db_server.session.commit()
+
+# subscriptions seed data
+def seed_subscriptions(db_server, subscription_db):
+  subscription_objects = [
+    subscription_db(subscription_name='-- Select Subscription --', rentals_allowed=0, price='0.00'),
+    subscription_db(subscription_name='Standard', rentals_allowed=1, price='4.99'),
+    subscription_db(subscription_name='Bronze', rentals_allowed=2, price='9.99'),
+    subscription_db(subscription_name='Silver', rentals_allowed=3, price='13.99'),
+    subscription_db(subscription_name='Gold', rentals_allowed=5, price='16.99'),
+    subscription_db(subscription_name='Platinum', rentals_allowed=7, price='18.99'),
+  ]
+  db_server.session.add_all(subscription_objects)
+  db_server.session.commit()
+
+# user seed data
+def seed_users(db_server, users_db):
+  user_objects = [
+    users_db(first_name='App', last_name='Admin', email='appadmin@ist412.io', address='17 Maxwell Road', city='Ruby Ridge', state='PA', zip_code=17745, password='1$VWDWKO3SQhZ53Hrg$bd7aeb560e215120e0892c4ab570ecc50eed1384eabba28c4cb7f7762e897c0364a67d13b46bf0595b180eee28a2b290480c00903818894e0487e01541e850c9', subscription_id=1, access_level=1),
+  ]
+  db_server.session.add_all(user_objects)
+  db_server.session.commit()
