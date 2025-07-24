d9247bf07329c1e05851d7903eb42ef2034df805
G. J. Yeomans <n8hth@protonmail.com>
2025-07-23 22:44:02 -0400

    Add initial forms controller script

---
+++ controllers/forms.py
@@ -0,0 +1,30 @@
+# controllers/forms.py
+
+# external imports
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, SelectField, StringField, SubmitField, ValidationError
+from wtforms.validators import DataRequired, Email, EqualTo
+
+## access forms
+
+class LoginForm(FlaskForm):
+  email = StringField('E-mail:', validators=[DataRequired(), Email()])
+  password = PasswordField('Password: ', validators=[DataRequired()])
+  submit = SubmitField('Login')
+
+class RegistrationForm(FlaskForm):
+  first_name = StringField('First Name: ', validators=[DataRequired()])
+  last_name = StringField('Last Name: ', validators=[DataRequired()])
+  address = StringField('Street Address: ', validators=[DataRequired()])
+  city = StringField('City: ', validators=[DataRequired()])
+  state = StringField('State: ', validators=[DataRequired()])
+  zip_code = StringField('Zip Code: ', validators=[DataRequired()])
+  email = StringField('E-mail: ', validators=[DataRequired(), Email()])
+  password = PasswordField('Password: ', validators=[DataRequired(), EqualTo('confirm_password', message='Passwords must match!')])
+  confirm_password = PasswordField('Confirm Password: ', validators=[DataRequired()])
+  subscription = SelectField('Subscription', choices=[], default=0)
+  submit = SubmitField('Create User')
+
+  def check_email(self, field):
+    if users.query.filter_by(email=field.data).first():
+      raise ValidationError('Your e-mail has already been registered.')
