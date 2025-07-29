4585cc4c043bc4b46ff4945a8f54977c99585ecc
G. J. Yeomans <n8hth@protonmail.com>
2025-07-28 21:59:00 -0400

    Initial (re)work on login process...more to come

--- controllers/access.py
+++ controllers/access.py
@@ -2,13 +2,13 @@
 
 # internal imports
 from .forms import LoginForm, RegistrationForm
-from models.models import db, users, subscriptions  
+from models.models import db, subscriptions, users
 # external imports
 import base64
 from cryptography.x509 import load_pem_x509_certificate
 from cryptography.hazmat.primitives import hashes, serialization
 from cryptography.hazmat.primitives.asymmetric import padding
-from flask import redirect, render_template, url_for
+from flask import redirect, render_template, session, url_for
 from pathlib import Path
 import json
 import jwt
@@ -20,7 +20,7 @@ import requests
 def validate_jwt(token):
   unverified_headers = jwt.get_unverified_header(token)
   x509_certificate = load_pem_x509_certificate(
-    Path("keys/public_key.pem").read_text().encode()
+    Path("keys/public_crt.pem").read_text().encode()
   ).public_key()
   return jwt.decode(
     token,
@@ -56,12 +56,32 @@ def login():
     # construct and send the user login post request
     login_request = requests.post(endpoint, data=json.dumps(credentials), headers=headers)
     userProfile = login_request.json()
-    logging.debug(userProfile)
-    #logging.debug(validate_jwt(userProfile['jwt_token']))
+    #logging.debug(userProfile)
+    if userProfile['message'] == "SUCCESS: User credentials authenticated!":
+      logging.debug(validate_jwt(userProfile['jwt_token']))
+      session["access_level"] = userProfile['access_level']
+      session["address"] = userProfile['address']
+      session["city"] = userProfile['city']
+      session["email"] = userProfile['email']
+      session["first_name"] = userProfile['first_name']
+      session["jwt_token"] = userProfile['jwt_token']
+      session["last_name"] = userProfile['last_name']
+      session["message"] = userProfile['message']
+      session["state"] = userProfile['state']
+      session["subscription_id"] = userProfile['subscription_id']
+      session["user_id"] = userProfile['user_id']
+      session["zip_code"] = userProfile['zip_code']
+    else:
+      return render_template('access/login.html', denied=userProfile['message'], message="Please login to GameFlix", form=form)
+    
+
+
+
+    #logging.debug(session)
     # redirect user to index afer successful login
     return redirect(url_for("index"))
   # open the loginform to be filled out by the user
-  return render_template('access/login.html', form=form)
+  return render_template('access/login.html', denied="", message="Please login to GameFlix", form=form,)
 
 def logout():
   session.clear()  # Wipe out user and its token cache from session
@@ -96,8 +116,8 @@ def register():
     # construct and send the new user post request
     new_user_request = requests.post(endpoint, data=json.dumps(newUser), headers=headers)
     userProfile = new_user_request.json
-    logging.debug(userProfile)
+    #logging.debug(userProfile)
     # redirect user to login form after successful registration
     return redirect(url_for('access.login'))
   # open the register form to be filled out by the user
-  return render_template('access/register.html', form=form)
+  return render_template('access/register.html', message="Complete form to join GameFlix", form=form)
