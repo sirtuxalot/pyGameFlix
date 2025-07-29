8d0191d7351d4f037db81d16079273732a8c37a5
G. J. Yeomans <n8hth@protonmail.com>
2025-07-28 21:53:48 -0400

    Minor changes to consoles seed data

--- seed_data.py
+++ seed_data.py
@@ -1,6 +1,7 @@
 # console seed data
 def seed_consoles(db_server, consoles_db):
   console_objects = [
+    consoles_db(console_name='-- Select Console --'),
     consoles_db(console_name='Nintendo Switch'),
     consoles_db(console_name='Nintendo Switch 2'),
     consoles_db(console_name='Nintendo Wii'),
@@ -9,8 +10,8 @@ def seed_consoles(db_server, consoles_db):
     consoles_db(console_name='Playstation 3'),
     consoles_db(console_name='Playstation 4'),
     consoles_db(console_name='Playstation 5'),
-    consoles_db(console_name='Playstation (PSP)'),
     consoles_db(console_name='Playstation (PS Vita)'),
+    consoles_db(console_name='Playstation (PSP)'),
     consoles_db(console_name='XBox'),
     consoles_db(console_name='XBox 360'),
     consoles_db(console_name='XBox One'),
