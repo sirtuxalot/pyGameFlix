26243dced31ffeaa516658d31326cb8d3233a759
G. J. Yeomans <n8hth@protonmail.com>
2025-08-08 12:32:24 -0400

    Update profile route path and add password and delete routes (#2)

--- routes/users.py
+++ routes/users.py
@@ -1,10 +1,12 @@
 # routes/users.py
 
 # internal imports
-from controllers.users import profile
+from controllers.users import delete, password, profile
 # external imports
 from flask import Blueprint
 
 users_bp = Blueprint('users', __name__)
 
-users_bp.route('/<user_id>', methods=['GET', 'POST'])(profile)
+users_bp.route('/profile/<user_id>', methods=['GET', 'POST'])(profile)
+users_bp.route('/password/<user_id>', methods=['GET', 'POST'])(password)
+users_bp.route('/delete/<user_id>', methods=['GET', 'POST'])(delete)
