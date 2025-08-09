76de42d7bedd2184c84a46733e89dc69f61c9685
G. J. Yeomans <n8hth@protonmail.com>
2025-08-09 18:07:24 -0400

    Initial work to manage catalog through management of consoles and subscriptions (#3)

--- controllers/admin_consoles.py
+++ controllers/admin_consoles.py
@@ -1,22 +1,56 @@
 # controllers/admin_consoles.py
   
 # internal imports
-from .forms import ConsoleForm
-from models.models import db, consoles
+from .admin_forms import ConsoleForm
+from models.models import db, catalog, consoles
 # external imports
-from flask import render_template
+from flask import redirect, render_template, request, session, url_for
 import logging
+from sqlalchemy import func
+from sqlalchemy.exc import DataError, IntegrityError
 
 def show_consoles():
-  form = ConsoleForm()
-  if form.validate_on_submit():
-    pass
-  consoles_query = db.session.query(consoles.console_id, consoles.console_name
-    ).order_by(consoles.console_name
-    ).filter(consoles.console_name.not_like('-- Select Console --'))
-  for console in consoles_query:
-    logging.debug("'%s' '%s'" % (console[0], console[1]))
-  return render_template('admin/consoles.html', consoles=consoles_query)
+  if "jwt_token" not in session:
+    return redirect(url_for("access.login"))
+  else:
+    form = ConsoleForm()
+    if form.validate_on_submit():
+      try:
+        new_console = consoles(
+          console_name=form.console_name.data)
+        db.session.add(new_console)
+        db.session.commit()
+        logging.info("New console: %s added!" % form.console_name.data)
+      except DataError:
+        db.session.rollback()
+        return render_template('exceptions.html', exception="Data Error: Divide by Zero, Value out of Range, etc...")
+      except IntegrityError:
+        db.session.rollback()
+        return render_template('exceptions.html', exception="Console already exists!")
+    game_count_query = db.session.query(catalog.console_id, func.count(catalog.console_id
+      ).label('game_count')
+      ).group_by(catalog.console_id
+      ).filter(catalog.console_id != 1)
+    game_count_subquery = game_count_query.subquery()
+    consoles_query = db.session.query(consoles.console_id, consoles.console_name, game_count_subquery.c.game_count
+      ).order_by(consoles.console_name
+      ).join(game_count_subquery, consoles.console_id == game_count_subquery.c.console_id, isouter=True
+      ).filter(consoles.console_name.not_like('-- Select Console --'))
+    for console in consoles_query:
+      logging.debug("'%s' '%s' '%s'" % (console[0], console[1], console[2]))
+    return render_template('admin/consoles.html', consoles=consoles_query, form=form)
 
 def edit_console(console_id):
-  pass
+  if "jwt_token" not in session:
+    return redirect(url_for("access.login"))
+  else:
+    console_edit = consoles.query.get_or_404(console_id)
+    form = ConsoleForm()
+    if form.validate_on_submit():
+      console_edit.console_name = form.console_name.data
+      db.session.commit()
+      logging.info("Console: %s updated!" % form.console_name.data)
+      return redirect(url_for('admin.show_consoles'))
+    elif request.method == 'GET':
+      form.console_name.data = console_edit.console_name
+    return render_template('admin/edit_console.html', form=form)
--- controllers/admin_subscriptions.py
+++ controllers/admin_subscriptions.py
@@ -1,23 +1,55 @@
 # controllers/admin_subscriptions.py
   
 # internal imports
-from .forms import SubscriptionForm
+from .admin_forms import SubscriptionForm
 from models.models import db, subscriptions
 # external imports
-from flask import render_template
+from flask import redirect, render_template, request, session, url_for
 import logging
 from sqlalchemy import asc
 
 def show_subscriptions():
-  form = SubscriptionForm()
-  if form.validate_on_submit():
-    pass
-  subscriptions_query = db.session.query(subscriptions.subscription_id, subscriptions.subscription_name, subscriptions.rentals_allowed, subscriptions.price
-    ).order_by(asc(subscriptions.rentals_allowed)
-    ).filter(subscriptions.subscription_name.not_like('-- Select Subscription --'))
-  for subscription in subscriptions_query:
-    logging.debug("'%s' '%s' '%s' '%s'" % (subscription[0], subscription[1], subscription[2], subscription[3]))
-  return render_template('admin/subscriptions.html', subscriptions=subscriptions_query)
+  if "jwt_token" not in session:
+    return redirect(url_for("access.login"))
+  else:
+    form = SubscriptionForm()
+    if form.validate_on_submit():
+      try:
+        new_subscription = subscriptions(
+          subscription_name=form.subscription_name.data,
+          rentals_allowed=form.rentals_allowed.data,
+          price=form.price.data)
+        db.session.add(new_subscription)
+        db.session.commit()
+        logging.info("New subscription level: %s added" % form.subscription_name.data)
+      except DataError:
+        db.session.rollback()
+        return render_template('exceptions.html', exception="Data Error: Divide by Zero, Value out of Range, etc...")
+      except IntegrityError:
+        db.session.rollback()
+        return render_template('exceptions.html', exception="subscription already exists!")
+    subscriptions_query = db.session.query(subscriptions.subscription_id, subscriptions.subscription_name, subscriptions.rentals_allowed, subscriptions.price
+      ).order_by(asc(subscriptions.rentals_allowed)
+      ).filter(subscriptions.subscription_name.not_like('-- Select Subscription --'))
+    for subscription in subscriptions_query:
+      logging.debug("'%s' '%s' '%s' '%s'" % (subscription[0], subscription[1], subscription[2], subscription[3]))
+    return render_template('admin/subscriptions.html', subscriptions=subscriptions_query, form=form)
 
 def edit_subscription(subscription_id):
-  pass
+  if "jwt_token" not in session:
+    return redirect(url_for("access.login"))
+  else:
+    subscription_edit = subscriptions.query.get_or_404(subscription_id)
+    form = SubscriptionForm()
+    if form.validate_on_submit():
+      subscription_edit.subscription_name = form.subscription_name.data
+      subscription_edit.rentals_allowed = form.rentals_allowed.data
+      subscription_edit.price = form.price.data
+      db.session.commit()
+      logging.info("Subscription: %s updated!" % form.subscription_name.data)
+      return redirect(url_for('admin.show_subscriptions'))
+    elif request.method == 'GET':
+      form.subscription_name.data = subscription_edit.subscription_name
+      form.rentals_allowed.data = subscription_edit.rentals_allowed
+      form.price.data = subscription_edit.price
+    return render_template('admin/edit_subscription.html', form=form)
